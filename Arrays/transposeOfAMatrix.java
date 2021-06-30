c8ade7bb2fc52a8e2919957c370a7963a47a4cfd
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-06-30 19:30:29 +0530

    Add Solutions

---
+++ Arrays/transposeOfAMatrix.java
@@ -0,0 +1,33 @@
+// Transpose Of A Matrix
+
+package Arrays;
+import java.util.Scanner;
+
+public class transposeOfAMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+        int[][] matrix = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        transposeOfAMatrix(row, col, matrix);
+    }
+
+    private static void transposeOfAMatrix(int row, int col, int[][] matrix) {
+        int[][] transposedMatrix = new int[col][row];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+               transposedMatrix[j][i] = matrix[i][j];
+            }
+        }
+        for (int i = 0; i < transposedMatrix.length; i++) {
+            for (int j = 0; j < transposedMatrix[0].length; j++) {
+                System.out.println(transposedMatrix[i][j]);
+            }
+        }
+    }
+}
---
+++ Arrays/twoSum.java
@@ -0,0 +1,22 @@
+//Two Sum
+
+package Arrays;
+import java.util.*;
+
+public class twoSum {
+    public static void main(String[] args) {
+        int[] arr = { 3,2,4 };
+        twoSum(arr, 6);
+    }
+
+    private static void twoSum(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            int currNum = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                if (currNum + arr[j] == target) {
+                    System.out.println(i + " " + j);
+                }
+            }
+        }
+    }
+}
