79639a20baf744aa355c12fae381517a60c4e396
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-07-04 23:33:43 +0530

    Add Solutions

---
+++ Arrays/BuyAndSellStocks2.java
@@ -0,0 +1,32 @@
+//Buy And Sell Stocks - Infinite Transactions Allowed
+
+package EasyQuestions.Arrays;
+import java.util.Scanner;
+
+public class BuyAndSellStocks2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] prices = new int[num];
+        for (int i = 0; i < prices.length; i++) {
+            prices[i] = sc.nextInt();
+        }
+        buyAndSellStocksMultipleTransactionsAllowed(prices);
+    }
+
+    private static void buyAndSellStocksMultipleTransactionsAllowed(int[] prices) {
+        int buyDate = 0;
+        int sellDate = 0;
+        int totalProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] >= prices[i - 1]) {
+                sellDate++;
+            } else {
+                totalProfit += prices[sellDate] - prices[buyDate];
+                sellDate = buyDate = i;
+            }
+        }
+        totalProfit += prices[sellDate] - prices[buyDate];
+        System.out.println(totalProfit);
+    }
+}
