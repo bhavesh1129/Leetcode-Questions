58596bab1794abdce95ae8ec31abcff6714aefcd
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-07-06 22:48:25 +0530

    Add Solution

---
+++ Strings/ExcelSheetColumnTitle.java
@@ -0,0 +1,23 @@
+//Excel Sheet Column Title
+
+package EasyQuestions.Strings;
+
+import java.util.Scanner;
+
+public class ExcelSheetColumnTitle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(excelSheetColumnTitle(num));
+    }
+
+    private static String excelSheetColumnTitle(int num) {
+        String result = "";
+        while (num > 0) {
+            num--;
+            result = (char) ('A' + (num % 26)) + result;
+            num = num / 26;
+        }
+        return result;
+    }
+}
