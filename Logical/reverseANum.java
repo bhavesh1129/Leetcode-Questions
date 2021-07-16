0c45b3ed7a56e5426d5c2897976a3168bffdf33a
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-07-17 00:07:31 +0530

    Add Solution

--- Logical/reverseANum.java
+++ Logical/reverseANum.java
@@ -14,12 +14,9 @@ public class reverseANum {
     private static int reverseANum(int num) {
         int ans = 0;
         while (num != 0) {
-            if (num > Integer.MAX_VALUE || num < Integer.MIN_VALUE) {
-                return num;
-            }
             ans = ans * 10 + num % 10;
             num /= 10;
         }
-        return ans;
+        return ans > Integer.MAX_VALUE || ans < Integer.MIN_VALUE ? 0 : (int) ans;
     }
 }
