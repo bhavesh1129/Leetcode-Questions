2fbb20d582b4b78dc84de602326a1933e1b0dda5
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-07-19 23:33:35 +0530

    Add Solution

---
+++ Matrix/minimuminRotatedSortedArray.java
@@ -0,0 +1,26 @@
+//Find Minimum in Rotated Sorted Array
+
+package EasyQuestions.Matrix;
+import java.util.*;
+
+public class minimuminRotatedSortedArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(minNumInSortedRotatedArr(arr));
+    }
+
+    private static int minNumInSortedRotatedArr(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+}
