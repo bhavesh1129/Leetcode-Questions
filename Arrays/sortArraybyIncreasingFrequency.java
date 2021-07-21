ae0fe68cd5668b9d7c7a5dbbdaa0fe5f8ade3456
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-07-22 00:46:58 +0530

    Add Solution

---
+++ Arrays/sortArraybyIncreasingFrequency.java
@@ -0,0 +1,20 @@
+//Sort Array by Increasing Frequency
+
+package EasyQuestions.Arrays;
+import java.util.Scanner;
+
+public class sortArraybyIncreasingFrequency {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sortArraybyIncreasingFrequency(num, arr);
+    }
+
+    private static void sortArraybyIncreasingFrequency(int num, int[] arr) {
+        
+    }
+}
