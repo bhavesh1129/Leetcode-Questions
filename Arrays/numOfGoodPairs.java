1f0fdc1702c3e63962cc837fe7219aec3331dc6b
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-08-14 20:17:52 +0530

    Add Solution

---
+++ Arrays/numOfGoodPairs.java
@@ -0,0 +1,29 @@
+//Number Of Good Pairs
+
+package EasyQuestions.Arrays;
+
+import java.util.*;
+
+public class numOfGoodPairs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(numOfGoodPairs(arr));
+    }
+
+    private static int numOfGoodPairs(int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
