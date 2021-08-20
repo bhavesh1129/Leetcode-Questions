52fa5ffb3268e4abe1bec02ab1f7bebbf4f56b8a
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-08-20 19:59:08 +0530

    Add Solution

---
+++ Arrays/peakIndexInMountainArray.java
@@ -0,0 +1,31 @@
+//Peak Index in a Mountain Array
+
+package EasyQuestions.Arrays;
+
+import java.util.Scanner;
+
+public class peakIndexInMountainArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(peakIndexInMountainArray(arr));
+    }
+
+    private static int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
