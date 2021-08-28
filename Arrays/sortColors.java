f5727295f1813c62e76d3a633158df865972ac3f
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-08-28 22:59:53 +0530

    Add Solution

---
+++ Arrays/sortColors.java
@@ -0,0 +1,43 @@
+//Sort Colors
+
+package EasyQuestions.Arrays;
+
+import java.util.*;
+
+public class sortColors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int[] res = sortColors(arr);
+        System.out.println(Arrays.toString(res));
+    }
+
+    private static int[] sortColors(int[] arr) {
+        int low = 0;
+        int mid = 0;
+        int high = arr.length - 1;
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                swap(arr, mid, low);
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else {
+                swap(arr, mid, high);
+                high--;
+            }
+        }
+        return arr;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
