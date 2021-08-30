a9dc776d4a3cb9b4f81affb9dd5d3cb538b6e7fb
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-08-30 14:55:46 +0530

    Add Solution

---
+++ Arrays/firstMissingPositive.java
@@ -0,0 +1,41 @@
+// First Missing Positive
+
+package EasyQuestions.Arrays;
+
+import java.util.Scanner;
+
+public class firstMissingPositive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int res = firstMissingPositive(arr);
+        System.out.println(res);
+    }
+
+    private static int firstMissingPositive(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            if (arr[i] > 0 && arr[i] <= arr.length && arr[i] != arr[arr[i] - 1]) {
+                swap(arr, i, arr[i] - 1);
+            } else {
+                i++;
+            }
+        }
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j] != j + 1) {
+                return j + 1;
+            }
+        }
+        return arr.length + 1;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
