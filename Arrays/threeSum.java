368e39482dbc6d4ba1ede3dc03c633983ca5da5f
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-08-30 14:53:55 +0530

    Add Solution

--- Arrays/threeSum.java
+++ Arrays/threeSum.java
@@ -44,8 +44,38 @@ public class threeSum {
                     j++;
                 }
             }
-
         }
+        // for (int i = 0; i < arr.length - 2; i++) {
+        //     if (i != 0 && arr[i] == arr[i - 1]) {
+        //         continue;
+        //     }
+        //     int start = i + 1;
+        //     int end = arr.length - 1;
+        //     while (start < end) {
+        //         int sum = arr[i] + arr[start] + arr[end];
+        //         if (sum > 0) {
+        //             end--;
+        //         } else if (sum < 0) {
+        //             start++;
+        //         } else {
+        //             ArrayList<Integer> list = new ArrayList<Integer>();
+        //             list.add(arr[i]);
+        //             list.add(arr[start]);
+        //             list.add(arr[end]);
+        //             res.add(list);
+
+        //             start++;
+        //             end--;
+
+        //             while (start < end && arr[start] == arr[start + 1]) {
+        //                 start++;
+        //             }
+        //             while (start < end && arr[end] == arr[end - 1]) {
+        //                 end--;
+        //             }
+        //         }
+        //     }
+        // }
         return res;
     }
 }
