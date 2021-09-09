e8e2bf377c02b11e084e67aabaaa077884746e1b
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-09-09 22:28:19 +0530

    Add Solution

--- Strings/validPalindrome.java
+++ Strings/validPalindrome.java
@@ -22,5 +22,23 @@ public class validPalindrome {
         }
         String rev = new StringBuffer(newStr).reverse().toString();
         return (newStr.equals(rev));
+
+        // int start = 0;
+        // int end = s.length() - 1;
+        // while (start <= end) {
+        // while (start <= end && !Character.isLetterOrDigit(s.charAt(start))) {
+        // start++;
+        // }
+        // while (start <= end && !Character.isLetterOrDigit(s.charAt(end))) {
+        // end--;
+        // }
+        // if (start <= end && Character.toLowerCase(s.charAt(start)) !=
+        // Character.toLowerCase(s.charAt(end))) {
+        // return false;
+        // }
+        // start++;
+        // end--;
+        // }
+        // return true;
     }
 }
