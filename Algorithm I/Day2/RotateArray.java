eeba731dce2c9278cbd393f9f38e01e922724b6e
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-10-25 22:53:15 +0530

    Add Solution

---
+++ Algorithm I/Day2/RotateArray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void rotate(int[] arr, int k) {
+        k %= arr.length;
+        reverse(arr, 0, arr.length - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, arr.length - 1);
+        
+    }
+    private static void reverse(int[] arr, int i, int j) {
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+
+            i++;
+            j--;
+        }
+    }
+}
