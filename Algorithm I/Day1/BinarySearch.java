cd78c07d3e1d5254e763002369f08e8aa34fcc2f
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-10-25 22:50:33 +0530

    Add Solution

---
+++ Algorithm I/Day1/BinarySearch.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(target == nums[mid]){
+                return mid;
+            }else if(target > nums[mid]){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
