af9793d18a7b9fc13a9af7b8fd14019740cf5038
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-11-04 10:47:03 +0530

    Add Solution

---
+++ LinkedList/SortList.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode merge(ListNode h1, ListNode h2){
+        if(h1 == null){
+            return h2;
+        }
+        if(h2 == null){
+            return h1;
+        }
+        
+        if(h1.val < h2.val){
+            h1.next = merge(h1.next, h2);
+            return h1;
+        }
+        else{
+            h2.next = merge(h1, h2.next);
+            return h2;
+        }
+    }
+    
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        
+        ListNode p1 = head;
+        ListNode p2 = head;
+        ListNode pre = head;
+        
+        while(p2 != null && p2.next != null){
+            pre = p1;
+            p1 = p1.next;
+            p2 = p2.next.next;
+        }
+        pre.next = null;
+        ListNode h1 = sortList(head);
+        ListNode h2 = sortList(p1);
+        return merge(h1, h2);  
+    }   
+}
