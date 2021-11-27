6b79e4cc3029aaa3864564d5ad7477bc1a4d2c63
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-11-27 13:04:20 +0530

    Update HouseRobber.java

--- DynamicProgramming/HouseRobber.java
+++ DynamicProgramming/HouseRobber.java
@@ -1,14 +1,20 @@
 class Solution {
     public int rob(int[] nums) {
         int curr = 0;
-        return rob(nums, curr, nums.length);
+        int n = nums.length;
+        int[] cache = new int[n + 1];
+        Arrays.fill(cache, -1);
+        return rob(nums, curr, n, cache);
     }
-    public int rob(int[] nums, int curr, int n){
-        if(curr >= n){
+    public int rob(int[] nums, int curr, int n, int[] cache){
+        if (curr >= n) {
             return 0;
         }
-        int ifRobbed = rob(nums, curr + 2, n) + nums[curr];
-        int ifNotRobbed = rob(nums, curr + 1, n);
-        return Math.max(ifRobbed, ifNotRobbed);
+        if (cache[curr] != -1) {
+            return cache[curr];
+        }
+        int ifRobbed = rob(nums, curr + 2, n, cache) + nums[curr];
+        int ifNotRobbed = rob(nums, curr + 1, n, cache);
+        return cache[curr] = Math.max(ifNotRobbed, ifRobbed);
     }
 }
