cc7620a8624e8580dbfda02cf4224150ed63e961
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2022-04-06 08:42:23 +0530

    Time: 1172 ms (10.08%), Space: 117.9 MB (8.87%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
@@ -1,31 +1,17 @@
 class Solution {
-    public int threeSumMulti(int[] arr, int target) {
-        Map<Integer, Long> map = new HashMap<>();
-        for (int i : arr) {
-            map.put(i, map.getOrDefault(i, 0l) + 1l);
-        }
-        System.out.println(map);
-        long ans = 0l;
-        for (int i : map.keySet()) {
-            for (int j : map.keySet()) {
-                int k = target - i - j;
-                if (map.containsKey(k)) {
-                    long freq1 = map.get(i);
-                    long freq2 = map.get(j);
-                    long freq3 = map.get(k);
-                    if (i == j && j == k) {
-                        ans += ((freq1) * (freq2 - 1) * (freq3 - 2)) / 6;
-                    }
-                    else if (i == j && i != k) {
-                        ans += ((freq1) * (freq2 - 1)) / 2 * freq3;
-                    }
-                    else if (i < j && j < k) {
-                        ans += freq1 * freq2 * freq3;
-                    }
-                }
-                ans = ans % 1000000007;
+    public int threeSumMulti(int[] A, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        int res = 0;
+        int mod = 1000000007;
+        for (int i = 0; i < A.length; i++) {
+            res = (res + map.getOrDefault(target - A[i], 0)) % mod;
+            
+            for (int j = 0; j < i; j++) {
+                int temp = A[i] + A[j];
+                map.put(temp, map.getOrDefault(temp, 0) + 1);
             }
         }
-        return (int) ans;
+        return res;
     }
 }
