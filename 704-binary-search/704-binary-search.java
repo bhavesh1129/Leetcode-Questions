465df67ca99aa53922befb0e261f0b8fe0fd739a
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2022-04-09 19:38:49 +0530

    Time: 0 ms (100.00%), Space: 54.9 MB (13.17%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int i = 0, j = nums.length - 1;
+        while(i <= j){
+            int mid = i + (j - i) / 2;
+            if(nums[mid] > target){
+                j = mid - 1;
+            }else if(nums[mid] < target){
+                i = mid + 1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
