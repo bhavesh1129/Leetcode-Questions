44c10087479cabc9a580f121459266efb8c473bb
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2022-04-22 09:11:29 +0530

    Time: 22 ms (74.66%), Space: 62.8 MB (45.41%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.java
+++ 706-design-hashmap/706-design-hashmap.java
@@ -1,21 +1,67 @@
 class MyHashMap {
-    int[] data;
-    
+    LinkedList<Entry>[] map;
+    public int SIZE = 769;
     public MyHashMap() {
-        this.data = new int[1000001];
-        Arrays.fill(data, -1);
+        map = new LinkedList[SIZE];
     }
     
-    public void put(int key, int value) {
-        data[key] = value;
+    public void put(int key, int val) {
+        int bucket = key % SIZE;
+        if(map[bucket] == null){
+            map[bucket] = new LinkedList<Entry>();
+            map[bucket].add(new Entry(key, val));
+        }else{
+            for(Entry entry: map[bucket]){
+                if(entry.key == key){
+                    entry.val = val;
+                    return;
+                }
+            }
+            map[bucket].add(new Entry(key, val));
+        }
     }
     
     public int get(int key) {
-        return data[key];
+        int bucket = key % SIZE;
+        LinkedList<Entry> entries = map[bucket];
+        if(entries == null){
+            return -1;
+        }
+        for(Entry entry : entries){
+            if(entry.key == key){
+                return entry.val;
+            }
+        }
+        return -1;
     }
     
     public void remove(int key) {
-        data[key] = -1;
+        int bucket = key % SIZE;
+        Entry removeKey = null;
+        if(map[bucket] == null){
+            return;
+        }else{
+            for(Entry entry: map[bucket]){
+                if(entry.key == key){
+                    removeKey = entry;
+                }
+            }
+            if(removeKey == null){
+                return;
+            }else{
+                map[bucket].remove(removeKey);
+            }
+        }
+    }
+    
+    class Entry{
+        public int key;
+        public int val;
+        
+        public Entry(int key, int val){
+            this.key = key;
+            this.val = val;
+        }
     }
 }
 
